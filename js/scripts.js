1144ce7a4dc96b20dbc788e975925ed9f2296caf
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-12-29 23:13:13 -0800

    minor fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,60 @@
-const menu = document.getElementById("menuParent")
+const menu = document.getElementById("menu")
+const menuParent = document.getElementById("menuParent")
 // note: items is an array
-const items = document.getElementsByClassName("item")
+// const items = document.getElementsByClassName("item")
 
-// function positioning() {
-//     for(i=0; i<items.length; i++) {
-//         items[i].style.translateY = "`${-(i*1000)}`"
-//     }
-// }
 
-// positioning()
-
-menu.addEventListener("click", function() {
-    for(i=0; i<items.length; i++) {
-        items[i].style.opacity = "1"
-    }
-
+menu.addEventListener("mouseover", function() {
     anime ({
         targets: "#item1",
         translateX: 60,
+        opacity: 2
         // delay: anime.stagger(100)
     })
 
     anime ({
         targets: "#item2",
         translateX: 120,
+        opacity: 2
     })
 
     anime ({
         targets: "#item3",
         translateX: 180,
+        opacity: 2
     })
 
     anime ({
         targets: "#item4",
         translateX: 240,
+        opacity: 2
+    })
+})
+
+menu.addEventListener("mouseout", function() {
+    anime ({
+        targets: "#item1",
+        translateX: 0,
+        opacity: -1 // had to set opacity to -1 to avoid weird stutter from bounceback effect in anime.js
+        // delay: anime.stagger(100)
+    })
+
+    anime ({
+        targets: "#item2",
+        translateX: 0,
+        opacity: -1
+    })
+
+    anime ({
+        targets: "#item3",
+        translateX: 0,
+        opacity: -1
+    })
+
+    anime ({
+        targets: "#item4",
+        translateX: 0,
+        opacity: -1
     })
 })
 
