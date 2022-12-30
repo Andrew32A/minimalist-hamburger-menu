531745ac4c59cf8f4ac4b2e61c946711461fd1b4
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-12-30 00:19:00 -0800

    added toggle on click and worked on background animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,107 @@
 const menu = document.getElementById("menu")
 const menuParent = document.getElementById("menuParent")
 // note: items is an array
-// const items = document.getElementsByClassName("item")
+const items = document.getElementsByClassName("item")
 
+let menuCounter = 0
 
-menu.addEventListener("mouseover", function() {
+function openMenu() {
+    anime({
+        targets: "#menu",
+        backgroundColor: "#4b5975",
+        width: "375px",
+        easing: "easeInOutQuad"
+    })
+
     anime ({
         targets: "#item1",
         translateX: 60,
-        opacity: 2
+        opacity: 2,
+        easing: "easeInOutQuad",
         // delay: anime.stagger(100)
     })
 
     anime ({
         targets: "#item2",
         translateX: 120,
-        opacity: 2
+        opacity: 2,
+        easing: "easeInOutQuad",
     })
 
     anime ({
         targets: "#item3",
         translateX: 180,
-        opacity: 2
+        opacity: 2,
+        easing: "easeInOutQuad",
     })
 
     anime ({
         targets: "#item4",
         translateX: 240,
-        opacity: 2
+        opacity: 2,
+        easing: "easeInOutQuad",
     })
-})
+}
 
-menu.addEventListener("mouseout", function() {
+function closeMenu() {
+    anime({
+        targets: "#menu",
+        backgroundColor: "#1b2028",
+        width: "0px",
+        easing: "easeOutQuint"
+    })
+
+    setTimeout(function() {
+        menu.style.width = "125px"
+    }, 500)
+
     anime ({
         targets: "#item1",
         translateX: 0,
-        opacity: -1 // had to set opacity to -1 to avoid weird stutter from bounceback effect in anime.js
+        opacity: -2 // had to set opacity to -2 to avoid weird stutter from bounceback effect in anime.js
         // delay: anime.stagger(100)
     })
 
     anime ({
         targets: "#item2",
         translateX: 0,
-        opacity: -1
+        opacity: -2
     })
 
     anime ({
         targets: "#item3",
         translateX: 0,
-        opacity: -1
+        opacity: -2
     })
 
     anime ({
         targets: "#item4",
         translateX: 0,
-        opacity: -1
-    })
-})
+        opacity: -2
+    })  
+}
+
+function menuToggle() {
+    menuCounter++
+    if (menuCounter % 2 === 0) {
+        closeMenu()
+    }
+    else {
+        openMenu()
+    }
+}
+
+menuParent.addEventListener("click", menuToggle)
+
+// adds animations to each item on mouseover and mouseout
+// for (let i = 0; i<items.length; i++) {
+//     items[i].addEventListener("mouseover", function() {
+//         anime ({
+//             targets: `${i}`,
+//             scale: 1.2
+//         })
+//     })
+// }
 
 // notes:
 
