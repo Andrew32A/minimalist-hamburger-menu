5a581b915ab18331ce5645145ccd25e419666ce1
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-12-31 13:49:24 -0800

    DRY coded anime.js and cleaned up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
-const menu = document.getElementById("menu")
 const menuParent = document.getElementById("menuParent")
-// note: items is an array
-const items = document.getElementsByClassName("item")
-
+const itemArray = ["#item1", "#item2", "#item3", "#item4"]
 let menuCounter = 0
 
 function openMenu() {
@@ -13,35 +10,16 @@ function openMenu() {
         easing: "easeInOutQuad"
     })
 
-    anime ({
-        targets: "#item1",
-        translateX: 60,
-        opacity: 2,
-        easing: "easeInOutQuad",
-        // i really wanted to use stagger to handle each item element, but it's only available on the latest version of anime.js which i couldn't get to work right now
-        // delay: anime.stagger(100)
-    })
-
-    anime ({
-        targets: "#item2",
-        translateX: 120,
-        opacity: 2,
-        easing: "easeInOutQuad",
-    })
-
-    anime ({
-        targets: "#item3",
-        translateX: 180,
-        opacity: 2,
-        easing: "easeInOutQuad",
-    })
-
-    anime ({
-        targets: "#item4",
-        translateX: 240,
-        opacity: 2,
-        easing: "easeInOutQuad",
-    })
+    for (let i = 0; i < itemArray.length; i++) {
+        anime ({
+            targets: itemArray[i],
+            translateX: (60 * (i + 1)),
+            opacity: 2,
+            easing: "easeInOutQuad"
+            // i really wanted to use stagger to handle each item element, but it's only available on the latest version of anime.js which i couldn't get to work right now
+            // delay: anime.stagger(100)
+        })
+    }
 }
 
 function closeMenu() {
@@ -52,34 +30,13 @@ function closeMenu() {
         easing: "easeOutQuint"
     })
 
-    // setTimeout(function() {
-    //     menu.style.width = "125px"
-    // }, 500)
-
-    anime ({
-        targets: "#item1",
-        translateX: 0,
-        opacity: -2 // had to set opacity to -2 to avoid weird stutter from bounceback effect in anime.js
-        // delay: anime.stagger(100)
-    })
-
-    anime ({
-        targets: "#item2",
-        translateX: 0,
-        opacity: -2
-    })
-
-    anime ({
-        targets: "#item3",
-        translateX: 0,
-        opacity: -2
-    })
-
-    anime ({
-        targets: "#item4",
-        translateX: 0,
-        opacity: -2
-    })  
+    for (let i = 0; i < itemArray.length; i++) {
+        anime ({
+            targets: itemArray[i],
+            translateX: 0,
+            opacity: -2 // had to set opacity to -2 to avoid weird stutter from bounceback effect in anime.js
+        })
+    }
 }
 
 function menuToggle() {
@@ -93,40 +50,3 @@ function menuToggle() {
 }
 
 menuParent.addEventListener("click", menuToggle)
-
-// adds animations to each item on mouseover and mouseout
-// for (let i = 0; i<items.length; i++) {
-//     items[i].addEventListener("mouseover", function() {
-//         anime ({
-//             targets: `${i}`,
-//             scale: 1.2
-//         })
-//     })
-// }
-
-// notes:
-
-// menu.addEventListener("mouseover", bubble)
-
-// function openBubble() {
-//     anime ({
-//         targets: "#menuParent, #item1",
-//         width: "50%",
-//         easing: "easeInOutQuad",
-//         // loop: true,
-//         // delay: 1,
-//         duration: 1000
-//     })
-// }
-
-// function closeBubble() {
-//     anime ({
-//         targets: "#menuParent, #item1",
-//         width: "0%",
-//         easing: "easeInOutQuad",
-//         // loop: true,
-//         // delay: 1,
-//         duration: 1000
-//     }) 
-// }
-// import anime from './anime.js';
